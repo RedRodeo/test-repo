5095daac7e9f070d28555b7bc0020abbe99b0416
RedRodeo <62592327+RedRodeo@users.noreply.github.com>
2020-05-27 00:09:18 +0530

    Addition List

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-from add import add
+from add import add, add_n_numbers
 a= int(input("Enter first number :"))
 b= int(input("Enter second number :"))
 print (add(a,b))
+n= eval(input("Enter List"))
+print(add_n_numbers(n))
